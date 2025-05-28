e3c7fda94a7d23120e05a161b377252474a941e1
JSirna <whitewolfhunter13@yahoo.com>
2025-05-27 20:43:36 -0400

    cleanup - add python utility - remove old utilities

---
+++ Python/csv2json.py
@@ -0,0 +1,43 @@
+import json
+import csv
+import os
+import tkinter.filedialog
+
+def convert_to_json(filepath):
+
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f"The file {filepath} does not exist.")
+
+    json_data = []
+    outputPath = os.path.splitext(filepath)[0] + '.json'
+    count = 0
+    try:
+        with open(filepath, 'r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            # Count rows for progress tracking
+            for row in csv_reader:
+                count += 1
+            print(count)
+            for row in csv_reader:
+                json_data.append(row)
+    except Exception as e:
+        raise RuntimeError(f"Error reading the CSV file: {e}")
+
+    try:
+        with open(outputPath, 'w') as json_file:
+            json.dump(json_data, json_file, indent=4)
+        print(f"Converted {filepath} to {outputPath}")
+    except Exception as e:
+        raise RuntimeError(f"Error writing to JSON file: {e}")
+    
+if __name__ == "__main__":
+    currentPath = os.path.dirname(os.path.abspath(__file__))
+    try:
+        file = tkinter.filedialog.askopenfile(mode='r', filetypes=[('CSV files', '*.csv')])
+        if file:
+            filepath = file.name
+            convert_to_json(filepath)
+        else:
+            print("No file selected.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Tools
-Container for tools like scrapers, utilities, & frontend components
+# Utilities
+Space for tools like scrapers, utilities, & frontend components
+
+## Python
+`csv2json.py`- a csv to json file converter using a simple tkinter file upload dialog  
+- How to use:
+    - Run the file with terminal of choice (VSCode, bash, cmd, powershell, etc.)
+    - Select a csv file
+    - Wait for processing to complete
+
+- Test Cases:
+    - `white_wine.csv` - taken from [Penn State Science](https://online.stat.psu.edu/stat857/node/223/)
+    - `wine.csv` - taken from [Penn State Science](https://online.stat.psu.edu/stat857/node/223/)
+    - `Crime_data_from_2020_to_Present.csv` - taken from [Data.gov](https://catalog.data.gov/dataset/crime-data-from-2020-to-present)
+
+- Next steps:
+    - Add progress bar for large datasets
+    - Performance checking
